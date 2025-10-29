9d92f8a52e4f4a1b3673108120856c31bb9caecc
Avinesh sharma <142591660+avineshsharma@users.noreply.github.com>
2025-10-29 19:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
+  this.make = make;
+  this.model = model;
 }
 
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
 function SportsCar(make, model, topSpeed) {
-	super(make);
-	super(model);
-	this.topSpeed = topSpeed;
+  Car.call(this, make, model); // ✅ correct way to call parent constructor
+  this.topSpeed = topSpeed;
 }
+
+SportsCar.prototype = Object.create(Car.prototype); // ✅ inheritance
+SportsCar.prototype.constructor = SportsCar;        // ✅ fix constructor reference
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
